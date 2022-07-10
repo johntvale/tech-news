4cfbfc6f109c550ac65e6071ed265f8ac6ee2ce9
johntvale <johntvale@gmail.com>
2022-07-10 15:40:52 -0400

    Requisito 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -63,4 +63,10 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    if type(category) != str:
+        return []
+    else:
+        news = search_news({
+            "category": {"$regex": category, "$options": "i"}
+        })
+        return get_tuple_list(news)
