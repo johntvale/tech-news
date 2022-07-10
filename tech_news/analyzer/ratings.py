c777f6d661d660c04da1b5c33225c372ea765ff1
johntvale <johntvale@gmail.com>
2022-07-10 17:10:10 -0400

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,13 @@
+from tech_news.analyzer.search_engine import get_tuple_list
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    sorted_news = get_collection().find({}).sort("comments_count", -1)
+    top_five = get_tuple_list(list(sorted_news)[:5])
+
+    return top_five
 
 
 # Requisito 11
