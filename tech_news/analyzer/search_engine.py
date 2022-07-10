f448068a823837d73c43f78cd4bc99f96ea05227
johntvale <johntvale@gmail.com>
2022-07-10 15:16:25 -0400

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -9,14 +10,48 @@ def search_by_title(title):
         news = search_news({"title": {"$regex": title, "$options": "i"}})
         output_list = []
         for each_news in news:
-            # print(each_news)
             output_list.append((each_news["title"], each_news["url"]))
         return output_list
 
 
+def new_format_date(date):
+    months = {
+        1: "janeiro",
+        2: "fevereiro",
+        3: "março",
+        4: "abril",
+        5: "maio",
+        6: "junho",
+        7: "julho",
+        8: "agosto",
+        9: "setembro",
+        10: "outubro",
+        11: "novembro",
+        12: "dezembro"
+    }
+
+    year = date.year
+    month = months[date.month]
+    day = date.day
+
+    return f"{day} de {month} de {year}"
+
+
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_format = datetime.fromisoformat(date).date()
+        new_date = new_format_date(date_format)
+        print(new_date)
+        news = search_news({
+            "timestamp": {"$regex": new_date, "$options": "i"}
+        })
+        output_list = []
+        for each_news in news:
+            output_list.append((each_news["title"], each_news["url"]))
+        return output_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
