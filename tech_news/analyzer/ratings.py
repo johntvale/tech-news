fe48c8a7b2da54b7a97b25a8877d979b60d2a28c
johntvale <johntvale@gmail.com>
2022-07-10 19:55:14 -0400

    Requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -4,12 +4,31 @@ from tech_news.database import get_collection
 
 # Requisito 10
 def top_5_news():
-    sorted_news = get_collection().find({}).sort("comments_count", -1)
-    top_five = get_tuple_list(list(sorted_news)[:5])
+    sorted_news = get_collection().find({}).sort("comments_count", -1).limit(5)
+    top_five = get_tuple_list(sorted_news)
 
     return top_five
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = get_collection().find({})
+    category_count = {}
+
+    for each_news in news:
+        if each_news["category"] in category_count:
+            category_count[each_news["category"]] += 1
+        else:
+            category_count[each_news["category"]] = 1
+
+    sorted_by_quantity = sorted(
+        category_count,
+        key=lambda x: category_count[x],
+        reverse=True,
+    )
+
+    return sorted_by_quantity[:5]
+
+# referencias da orderação:
+# https://careerkarma.com/blog/python-sort-a-dictionary-by-value/#:~:text=To%20sort%20a%20dictionary%20by%20value%20in%20Python%20you%20can,Dictionaries%20are%20unordered%20data%20structures.
+# https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
